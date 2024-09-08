0ed16cd2adcea13f758868e844ecc362c1c05bca
Sumedh Meher Pophali <sumedhmeher.pophali@dish.com>
2024-09-08 15:53:32 +0530

    added up/down icon and description in expanded state

--- app/src/main/java/com/example/composehandsonapp/MainActivity.kt
+++ app/src/main/java/com/example/composehandsonapp/MainActivity.kt
@@ -3,6 +3,7 @@ package com.example.composehandsonapp
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.animation.animateContentSize
 import androidx.compose.animation.core.Spring
 import androidx.compose.animation.core.animateDpAsState
 import androidx.compose.animation.core.animateFloatAsState
@@ -28,7 +29,13 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.foundation.lazy.items
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.ExpandLess
+import androidx.compose.material.icons.filled.ExpandMore
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
 import androidx.compose.runtime.saveable.rememberSaveable
+import androidx.compose.ui.res.stringResource
 import com.example.composehandsonapp.ui.theme.ComposeHandsOnAppTheme
 
 class MainActivity : ComponentActivity() {
@@ -116,22 +123,45 @@ fun Greeting(name: String, modifier: Modifier = Modifier) {
 
     Surface(
         color = MaterialTheme.colorScheme.primary,
-        modifier = modifier.padding(vertical = 4.dp, horizontal = 8.dp)
+        modifier = modifier
+            .padding(vertical = 4.dp, horizontal = 8.dp)
+            .animateContentSize(
+                animationSpec = spring(
+                    dampingRatio = Spring.DampingRatioHighBouncy,
+                    stiffness = Spring.StiffnessLow
+                )
+            )
     ) {
         Row(modifier = Modifier.padding(24.dp)) {
             Column(
                 modifier = Modifier
                     .weight(1f)
-                    .padding(bottom = extraPadding.coerceAtLeast(0.dp))
+                    .padding(12.dp)
             ) {
                 Text(text = "Hello, ")
                 Text(text = name)
+                
+                if (expanded){
+                    Text(text = ("Its been so long").repeat(4))
+                }
             }
-            ElevatedButton(
-                onClick = { expanded = !expanded }
-            ) {
-                Text(if (expanded) "Show less" else "Show more")
+            
+            IconButton(onClick = { expanded = !expanded }) {
+
+                Icon(
+                    imageVector = if (expanded) Icons.Filled.ExpandLess else Icons.Filled.ExpandMore,
+                    contentDescription = if (expanded) {
+                        stringResource(R.string.show_less)
+                    } else {
+                        stringResource(R.string.show_more)
+                    }
+                )
             }
+//            ElevatedButton(
+//                onClick = { expanded = !expanded }
+//            ) {
+//                Text(if (expanded) "Show less" else "Show more")
+//            }
         }
     }
 }
